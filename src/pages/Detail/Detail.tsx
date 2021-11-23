0029e02143dd94dfdbba288bc9d7589787004aaa
Ricard Garcia <imricardgarcia@gmail.com>
2021-11-23 17:51:18 +0100

    feat: added detail main containers

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+// Components
+import Layout from "../../components/Layout";
+
 export default function Detail() {
   return (
-    <div>
-      <h1>Detail</h1>
-    </div>
+    <Layout>
+      {/* Top */}
+      <div id="detailTop" className="flex-between-center px-5 pb-5">
+        <h1 className="text-uppercase">Movie/Show name</h1>
+      </div>
+      {/* Bottom */}
+      <div id="detailBottom">
+        <p>Content goes here</p>
+      </div>
+    </Layout>
   );
 }
